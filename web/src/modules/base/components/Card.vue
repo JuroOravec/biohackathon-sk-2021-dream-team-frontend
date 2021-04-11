0ee414bd660002b7a35be68ba1d26b705504019a
Juro Oravec <juraj.oravec.josefson@gmail.com>
2021-04-11 15:37:35 +0100

    feat: add homepage

---
+++ web/src/modules/base/components/Card.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card class="Card elevation-3" :class="{ 'Card--no-title': !$slots['title'] }">
+    <v-row class="Card__title">
+      <v-col cols="auto" class="px-0">
+        <v-card-title>
+          <slot name="title" />
+        </v-card-title>
+      </v-col>
+      <slot name="title-append" />
+    </v-row>
+
+    <v-card-text class="Card__body">
+      <v-row>
+        <slot />
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+const Card = defineComponent({
+  name: 'Card',
+});
+
+export default Card;
+</script>
+
+<style lang="scss">
+@import '~vuetify/src/styles/main';
+.Card {
+  padding: 3 * $spacer;
+
+  & + & {
+    margin-top: 8 * $spacer;
+  }
+
+  &__title {
+    margin: 3 * $spacer 0;
+
+    .col {
+      padding-bottom: 0;
+      padding-top: 0;
+    }
+    .v-card__title {
+      padding-bottom: 0;
+      padding-top: 0;
+    }
+  }
+
+  &--no-title {
+    .Card__title {
+      margin-top: 0;
+      margin-bottom: 0;
+    }
+  }
+}
+</style>
--- web/src/modules/base/components/Home.vue
+++ web/src/modules/base/components/Home.vue
@@ -1,42 +1,69 @@
 <template>
   <div class="Home">
-    <h1 class="text-h3 text-sm-h2 font-weight-bold py-10">MoovinGroovin</h1>
-    <h2 class="text-h4 pb-10">
-      Run wild with your sport music selection. <br />
-      We will save it for you.
-    </h2>
+    <v-img src="../assets/bg-banner-1.png" contain />
 
-    MoovinGroovin creates Spotify playlists from songs you played during your Strava activities.
-    Enjoy the moment, knowing you won't have to stop every time you want to save a song.
-
-    <h3 class="text-h5 pt-12 pb-4">Save the songs that keep you moving</h3>
-
-    <ol>
-      <li>Login and connect your Strava and Spotify accounts to MoovinGroovin.</li>
-      <li>Go for a run or bike. Start a Strava activity and put on your fave songs on Spotify!</li>
-      <li>When done, save your activity.</li>
-      <li>
-        Take a nice shower, drink a glass of water and relax. In 5-30 mins, a new playlist will be
-        created for you with the songs you listened to during the activity.
-      </li>
-    </ol>
-
-    <div class="pt-12 pb-4">
-      <LightboxImg
-        :src="require('../assets/example-spotify.png')"
-        alt="Example Spotify playlist"
-        :max-width="600"
-      />
-    </div>
-
-    <h3 class="text-h5 pt-12 pb-4">How it works</h3>
-
-    <p>MoovinGroovin integrates with your Strava and Spotify accounts.</p>
-    <p class="pb-8">
-      When you finish an activity, MoovinGroovin receives a message from Strava. MoovinGroovin then
-      asks Spotify for songs played at that time. New Spotify playlist is then created from those
-      songs.
-    </p>
+    <v-row class="Home__cards">
+      <v-col>
+        <router-link :to="{ name: DashboardRoute.ROOT }">
+          <Card>
+            <v-img
+              src="../assets/microbiome-gut-1.png"
+              contain
+              :max-height="200"
+              :max-width="200"
+            />
+          </Card>
+          <p class="text-body-1 font-weight-bold text-center pa-2">Črevný Mikrobióm</p>
+        </router-link>
+      </v-col>
+      <v-col>
+        <router-link :to="{ name: DashboardRoute.ROOT }">
+          <Card>
+            <v-img
+              src="../assets/microbiome-skin-1.png"
+              contain
+              :max-height="200"
+              :max-width="200"
+            />
+          </Card>
+          <p class="text-body-1 font-weight-bold text-center pa-2">Kožný Mikrobióm</p>
+        </router-link>
+      </v-col>
+      <v-col>
+        <router-link :to="{ name: DashboardRoute.ROOT }">
+          <Card>
+            <v-img
+              src="../assets/microbiome-oral-1.png"
+              contain
+              :max-height="200"
+              :max-width="200"
+            />
+          </Card>
+          <p class="text-body-1 font-weight-bold text-center pa-2">Ústny Mikrobióm</p>
+        </router-link>
+      </v-col>
+      <v-col>
+        <router-link :to="{ name: DashboardRoute.ROOT }">
+          <Card>
+            <v-img
+              src="../assets/microbiome-vaginal-1.png"
+              contain
+              :max-height="200"
+              :max-width="200"
+            />
+          </Card>
+          <p class="text-body-1 font-weight-bold text-center pa-2">Vaginálny Mikrobióm</p>
+        </router-link>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="text-center">
+        <v-img src="../../dashboard/assets/bg-1.png" :max-height="250">
+          <h3 class="text-h5 white--text">Spoznajte Vaše baktérie</h3>
+          <v-btn class="my-8" :to="{ name: DashboardRoute.ROOT }"> Chcem zistiť viac </v-btn>
+        </v-img>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -44,11 +71,19 @@
 import { defineComponent } from '@vue/composition-api';
 
 import LightboxImg from '@/modules/utils/components/LightboxImg.vue';
+import Card from './Card.vue';
+import { DashboardRoute } from '../../dashboard/types';
 
 const Home = defineComponent({
   name: 'Home',
   components: {
     LightboxImg,
+    Card,
+  },
+  setup() {
+    return {
+      DashboardRoute,
+    };
   },
 });
 
@@ -58,6 +93,21 @@ export default Home;
 <style lang="scss">
 @import 'vuetify/src/styles/styles';
 .Home {
+  &__cards {
+    padding-top: 100px;
+    padding-bottom: 100px;
+  }
+
+  .Card {
+    height: 200px;
+  }
+
+  .v-responsive__content {
+    width: 100% !important;
+    height: 100% !important;
+    align-self: center;
+  }
+
   li {
     padding-bottom: $spacer * 2;
 
